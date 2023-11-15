9db49cf9f6dc808f1f4875cb6ebc9227b35a12ba
Mopidevi Karthik <20501a5b7@pvpsit.ac.in>
2023-11-15 10:10:04 +0530

    initial commit

--- ui/src/axios.js
+++ ui/src/axios.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    baseURL: 'https://tourism-backend-ce6w.onrender.com',
-    // baseURL: 'http://localhost:4201',
-    headers: {
-        contentType: 'application/json',
-        Accept: 'application/json',
-        // 'Access-Control-Allow-Origin': 'https//localhost:3000'
-        'Access-Control-Allow-Origin': 'https://tourism-6ppu.onrender.com'
-        
-    }
-})
+  baseURL: "https://tourism-backend-ce6w.onrender.com",
+  // baseURL: 'http://localhost:4201',
+  headers: {
+    contentType: "application/json",
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "https//localhost:3000",
+    // 'Access-Control-Allow-Origin': 'https://tourism-6ppu.onrender.com'
+  },
+});
 
-export {Axios}
+export { Axios };
